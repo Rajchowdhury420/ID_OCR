401f01adf0d1c1e15dd9ae4828a9bd031b5c68c9
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2019-09-10 18:21:59 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -7,7 +7,6 @@ import cv2
 import os
 import imutils
 
-
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,help="path to input image to be OCR'd")
 args = vars(ap.parse_args())
